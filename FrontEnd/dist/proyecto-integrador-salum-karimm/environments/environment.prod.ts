b9e94ec590e1c8b9745b44bd6988fea006a2e6f0
Karimsalum <99426108+Karimsalum@users.noreply.github.com>
2022-10-31 23:41:39 -0300

    Add files via upload

---
+++ FrontEnd/dist/proyecto-integrador-salum-karimm/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'karimargentinaprograma',
+    appId: '1:945308169635:web:bc08fcc44b87bac8699616',
+    storageBucket: 'karimargentinaprograma.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCieCY3hkEyLlszXI9mBLq6RXYnDC7W1-E',
+    authDomain: 'karimargentinaprograma.firebaseapp.com',
+    messagingSenderId: '945308169635',
+  },
+  production: true,
+  URL: 'https://karimrafaelsalum.web.app/'
+};
---
+++ FrontEnd/dist/proyecto-integrador-salum-karimm/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'karimargentinaprograma',
+    appId: '1:945308169635:web:bc08fcc44b87bac8699616',
+    storageBucket: 'karimargentinaprograma.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCieCY3hkEyLlszXI9mBLq6RXYnDC7W1-E',
+    authDomain: 'karimargentinaprograma.firebaseapp.com',
+    messagingSenderId: '945308169635',
+  },
+  production: true,
+  //URL: 'http://localhost:8080/'
+  URL: 'https://karimrafaelsalum.web.app/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
